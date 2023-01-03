f59da45b56fec3d246a1154a709e2094bd57c63b
ah-showrov <ahshowrov1@gmail.com>
2023-01-03 23:36:54 +0600

    react-chart-2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DoughnutChart from './Charts/Doughnut';
+
+function App() {
+  return (
+    <div className="App">
+     <DoughnutChart/>
+
+    </div>
+  );
+}
+
+export default App;
